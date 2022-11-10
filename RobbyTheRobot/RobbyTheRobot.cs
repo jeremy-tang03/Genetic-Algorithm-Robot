2d2676e52f29bd2d35e5e0d09cd7535d582b0e06
RaphyBoy <raphaelcanciani@gmail.com>
2022-11-10 10:56:41 -0500

    Started the robby the robot class, need to wait for the geneticalgorithm to continue

--- RobbyTheRobot/RobbyTheRobot.cs
+++ RobbyTheRobot/RobbyTheRobot.cs
@@ -1,6 +1,35 @@
 namespace RobbyTheRobot
 {
-    internal class RobbyTheRobot:IRobbyTheRobot{
+    internal class RobbyTheRobot : IRobbyTheRobot
+    {
+        public int NumberOfActions { get; }
 
+        public int NumberOfTestGrids { get; }
+
+        public int GridSize { get; }
+
+        public int NumberOfGenerations { get; }
+
+        public double MutationRate { get; }
+
+        public double EliteRate { get; }
+
+        public RobbyTheRobot(int numberOfGenerations, int populationSize, int numberOfTrials, int? seed){
+            this.NumberOfGenerations = numberOfGenerations;
+            //not sure if this is right
+            this.NumberOfActions = numberOfTrials;
+            // not sure for populationSize
+            //not sure for seed
+        }
+
+        public void GeneratePossibleSolutions(string folderPath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ContentsOfGrid[,] GenerateRandomTestGrid()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
