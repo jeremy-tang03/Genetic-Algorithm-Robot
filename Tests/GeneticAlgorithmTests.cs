a4c3008dc3b458b981bc48eea89d91eaaea9346e
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-11-10 12:17:32 -0500

    add GeneticAlgorithmTests.cs

---
+++ Tests/GeneticAlgorithmTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeneticAlgorithm;
+
+namespace Tests
+{
+    [TestClass]
+    public class GeneticAlgorithmTests
+    {
+        [TestMethod]
+        public void Test(){
+            
+        }
+    }
+}
