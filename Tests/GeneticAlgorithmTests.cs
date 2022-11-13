67d535c40ecc6703a8748169ae1c65b7f197d33d
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-11-12 22:02:49 -0500

    add valid constructor test

--- Tests/GeneticAlgorithmTests.cs
+++ Tests/GeneticAlgorithmTests.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GeneticAlgorithm;
+using GA = GeneticAlgorithm;
 
 namespace Tests
 {
@@ -7,7 +7,20 @@ namespace Tests
     public class GeneticAlgorithmTests
     {
         [TestMethod]
-        public void Test(){
+        public void TestConstructorValidParams(){
+            GA.GeneticAlgorithm algo = new GA.GeneticAlgorithm(200, 200, 200, 5, 10, 2, null, null);
+            Assert.AreEqual(algo.PopulationSize, 200);
+            Assert.AreEqual(algo.NumberOfGenes, 200);
+            Assert.AreEqual(algo.LengthOfGene, 200);
+            Assert.AreEqual(algo.MutationRate, 5);
+            Assert.AreEqual(algo.EliteRate, 10);
+            Assert.AreEqual(algo.NumberOfTrials, 2);
+            Assert.AreEqual(algo.FitnessCalculation, null);
+            Assert.AreEqual(algo.Seed, null);
+        }
+
+        [TestMethod]
+        public void TestConstructorInvalidParams(){
             
         }
     }
