002b8ddf025e181643a13fa991c214a2fef0a331
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-11-14 12:11:29 -0500

    update code to use GridSize as side and not area

--- RobbyTheRobot/RobbyTheRobot.cs
+++ RobbyTheRobot/RobbyTheRobot.cs
@@ -23,7 +23,7 @@ namespace RobbyTheRobot
             this.NumberOfActions = numberOfTrials;
             // not sure for populationSize
             //not sure for seed
-            this.GridSize = 100; // TODO: temporary, for testing
+            this.GridSize = 10;
         }
 
         public void GeneratePossibleSolutions(string folderPath)
@@ -33,14 +33,11 @@ namespace RobbyTheRobot
 
         public ContentsOfGrid[,] GenerateRandomTestGrid()
         {
-            // assuming grid is square
-            int side = (int)Math.Floor(Math.Sqrt(this.GridSize));
-            int gridArea = side * side;
-            ContentsOfGrid[,] contents = new ContentsOfGrid[side, side];
+            ContentsOfGrid[,] contents = new ContentsOfGrid[this.GridSize, this.GridSize];
             Random random = new Random();
             int canCount = 0;
             int emptyCount = 0;
-            int half = (int)Math.Floor((double)gridArea / 2);
+            int half = contents.Length / 2;
 
             for (int i = 0; i < contents.GetLength(0); i++)
             {
