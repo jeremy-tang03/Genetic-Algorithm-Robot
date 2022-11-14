642490ecc3bdfc9a5b957282fbfdd3962ea1dcb8
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-11-13 20:35:26 -0500

    add random grid generation

--- RobbyTheRobot/RobbyTheRobot.cs
+++ RobbyTheRobot/RobbyTheRobot.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace RobbyTheRobot
 {
     internal class RobbyTheRobot : IRobbyTheRobot
@@ -14,7 +16,8 @@ namespace RobbyTheRobot
 
         public double EliteRate { get; }
 
-        public RobbyTheRobot(int numberOfGenerations, int populationSize, int numberOfTrials, int? seed){
+        public RobbyTheRobot(int numberOfGenerations, int populationSize, int numberOfTrials, int? seed)
+        {
             this.NumberOfGenerations = numberOfGenerations;
             //not sure if this is right
             this.NumberOfActions = numberOfTrials;
@@ -29,7 +32,26 @@ namespace RobbyTheRobot
 
         public ContentsOfGrid[,] GenerateRandomTestGrid()
         {
-            throw new System.NotImplementedException();
+            // assuming grid is square
+            int side = (int)Math.Floor(Math.Sqrt(this.GridSize));
+            ContentsOfGrid[,] contents = new ContentsOfGrid[side, side];
+            Random random = new Random();
+
+            for (int i = 0; i < contents.GetLength(0); i++)
+            {
+                for (int j = 0; j < contents.GetLength(1); j++)
+                {
+                    int num = random.Next(2);
+                    if (num == 0)
+                    {
+                        contents[i, j] = ContentsOfGrid.Empty;
+                    } else {
+                        contents[i, j] = ContentsOfGrid.Can;
+                    }
+                }
+            }
+
+            return contents;
         }
     }
 }
