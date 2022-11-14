f1f6c371b91318a381f4dccc2c4a24ad76d5071b
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-11-13 20:46:29 -0500

    update method for 50-50

--- RobbyTheRobot/RobbyTheRobot.cs
+++ RobbyTheRobot/RobbyTheRobot.cs
@@ -34,21 +34,34 @@ namespace RobbyTheRobot
         {
             // assuming grid is square
             int side = (int)Math.Floor(Math.Sqrt(this.GridSize));
+            int gridArea = side*side;
             ContentsOfGrid[,] contents = new ContentsOfGrid[side, side];
             Random random = new Random();
+            int canCount = 0;
 
             for (int i = 0; i < contents.GetLength(0); i++)
             {
                 for (int j = 0; j < contents.GetLength(1); j++)
                 {
                     int num = random.Next(2);
-                    if (num == 0)
+                    if (num == 0 && canCount <= Math.Floor((double)gridArea/2))
                     {
-                        contents[i, j] = ContentsOfGrid.Empty;
-                    } else {
                         contents[i, j] = ContentsOfGrid.Can;
+                        canCount++;
+                    } else {
+                        contents[i, j] = ContentsOfGrid.Empty;
                     }
                 }
+            }
+
+            Console.WriteLine("cans: " + canCount + ", grid area: " + gridArea);
+            for (int i = 0; i < contents.GetLength(0); i++)
+            {
+                for (int j = 0; j < contents.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", contents[i, j]);
+                }
+                Console.Write("\n");
             }
 
             return contents;
