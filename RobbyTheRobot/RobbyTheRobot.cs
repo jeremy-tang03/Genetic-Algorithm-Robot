55a3264f896cef60a245d48889470855b078a574
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-11-16 00:31:12 -0500

    update GeneratePossibleSolutions method

--- RobbyTheRobot/RobbyTheRobot.cs
+++ RobbyTheRobot/RobbyTheRobot.cs
@@ -39,21 +39,23 @@ namespace RobbyTheRobot
 
         public void GeneratePossibleSolutions(string folderPath) //TODO: loop next gens  
         {
-            // var topGene = GA.CurrentGeneration[0].Genes;
-            var gen = GA.CurrentGeneration as GenerationDetails;
-            // Console.WriteLine(gen.MaxFitness);
-
-            string lines = gen.MaxFitness + ", "+ gen[0].Genes.Length + ", " + gen[0].Genes + "\r\n";
-
-            // Write the string to a file.
-            System.IO.File.WriteAllText("C:\\Users\\Jeremy\\Desktop\\test.txt", lines);
-            // Open the file to read from.
+            string result = "";
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i == 1 || i == 20 || i == 100 || i == 200 || i == 500 || 1 == 1000)
+                {
+                    GenerationDetails gen = GA.CurrentGeneration as GenerationDetails;
+                    gen.EvaluateFitnessOfPopulation();
+                    result += gen.MaxFitness + ", " + gen[0].Genes.Length + ", " + gen[0].Genes + "\r\n";
+                }
+                GA.GenerateGeneration(); // or maybe run this before the if?
+            }
+            // Write string to file
+            System.IO.File.WriteAllText("C:\\Users\\Jeremy\\Desktop\\test.txt", result);
+            //TODO: to remove, code for testing purposes
+            // Open the file to read from. 
             string readText = System.IO.File.ReadAllText("C:\\Users\\Jeremy\\Desktop\\test.txt");
             Console.WriteLine(readText);
-
-            // System.IO.StreamWriter file = new System.IO.StreamWriter(folderPath);
-            // file.WriteLine(lines);
-            // file.Close();
         }
 
         public ContentsOfGrid[,] GenerateRandomTestGrid()
