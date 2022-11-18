24c948184d6ef4331a39de62dee24c695909c23e
RaphyBoy <raphaelcanciani@gmail.com>
2022-11-18 12:29:02 -0500

    started connecting frontend and backend

--- RobbyVisualizer/RobbyVisualizerGame.cs
+++ RobbyVisualizer/RobbyVisualizerGame.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using RobbyTheRobot;
 
 namespace RobbyVisualizer
 {
@@ -16,8 +18,13 @@ namespace RobbyVisualizer
 
         private SpriteFont _font;
 
-        private string _generationText = "Generation: 0";
+        private RobbyTheRobot.IRobbyTheRobot _robby;
 
+        private ContentsOfGrid[,] testGrid;
+
+        private string _generationText = "Generation: 1";
+
+        private int currentMove;
         private string _moveText = "Move: 0/200";
 
         private string _pointsText = "Points: 0/500";
@@ -41,7 +48,12 @@ namespace RobbyVisualizer
             _canTexture = this.Content.Load<Texture2D>("can");
             _robotTexture = this.Content.Load<Texture2D>("robot2");
 
+            _robby = Robby.createRobby(200,1,1,1,1,1,1,1,10);
+            testGrid = _robby.GenerateRandomTestGrid();
             _graphics.ApplyChanges();
+
+            currentMove = 0;
+
             base.Initialize();
         }
 
@@ -67,6 +79,15 @@ namespace RobbyVisualizer
             _spriteBatch.Begin();
             drawBoard();
 
+            if (currentMove == 0) {
+                Random rand = new Random();
+                int x = rand.Next(_robby.GridSize);
+                int y = rand.Next(_robby.GridSize);   
+                Vector2 position = new Vector2((x*70)+3, (y*70)+3);
+                drawRobby(position);
+                currentMove++;
+            }
+
             Vector2 generationTextPosition = new Vector2(30, 720);
             _spriteBatch.DrawString(_font, _generationText, generationTextPosition, Color.White);
             Vector2 moveTextPosition = new Vector2(30, 780);
@@ -74,9 +95,6 @@ namespace RobbyVisualizer
             Vector2 pointsTextPosition = new Vector2(30, 840);
             _spriteBatch.DrawString(_font, _pointsText, pointsTextPosition, Color.White);  
             
-            
-            Vector2 canPosition = new Vector2(0, 0);
-            _spriteBatch.Draw(_canTexture, canPosition, Color.White);
 
             //each tile is 70 x 70
             //add 3 pixels to each side to make it look better
@@ -84,11 +102,11 @@ namespace RobbyVisualizer
             _spriteBatch.Draw(_robotTexture, robotPosition, Color.White);
 
             //reset a tile
-            drawBlackTile(70, 70, 70);
+            //drawBlackTile(70, 70, 70);
 
             _spriteBatch.End();
 
-            base.Draw(gameTime);
+            base.Draw(gameTime);   
         }
 
         protected void drawBoard(){
@@ -100,6 +118,17 @@ namespace RobbyVisualizer
                 for (int j = 0; j < column; ++j)
                 {
                     drawTile(i * rectangleSideLength, j * rectangleSideLength, rectangleSideLength);
+                }
+            }
+
+            for (int i = 0; i < testGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < testGrid.GetLength(1); j++)
+                {
+                    if (testGrid[i, j] == ContentsOfGrid.Can){
+                        Vector2 posiiton = new Vector2(i*70, j*70); 
+                        drawCan(posiiton);
+                    }
                 }
             }
         }
@@ -121,8 +150,12 @@ namespace RobbyVisualizer
 
             drawTile(x, y, height);
         }
-        protected void drawCan(){
+        protected void drawCan(Vector2 position){
+            _spriteBatch.Draw(_canTexture, position, Color.White);
+        }
 
+        protected void drawRobby(Vector2 position){
+            _spriteBatch.Draw(_robotTexture, position, Color.White);
         }
     }
 }
