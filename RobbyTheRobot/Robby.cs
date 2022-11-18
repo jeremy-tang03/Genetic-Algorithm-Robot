8908542a1748a0d33b43a404c0d835d849097704
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-11-18 13:06:46 -0500

    work on backend and helping

--- RobbyTheRobot/Robby.cs
+++ RobbyTheRobot/Robby.cs
@@ -5,7 +5,7 @@ namespace RobbyTheRobot
     public class Robby
     {
         public static IRobbyTheRobot createRobby(int numberOfActions, int numberOfTestGrids, int numberOfGenerations, double mutationRate, double eliteRate, int populationSize, int numberOfGenes, int lengthOfGene, int numberOfTrials, int gridSize = 10, int? seed = null){
-            return new RobbyTheRobot(numberOfActions, numberOfTestGrids, numberOfGenerations, mutationRate, eliteRate, populationSize, numberOfGenes, lengthOfGene, numberOfTrials, gridSize, seed);
+            return new RobbyTheRobot(numberOfTestGrids, numberOfGenerations, mutationRate, eliteRate, populationSize, numberOfGenes, lengthOfGene, numberOfTrials, gridSize, seed, numberOfActions);
         }
     }
 }
